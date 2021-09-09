36537fe5555c9c31df97aff33e59836cf31bd603
Pedro Ferreira <pedroferrodude@hotmail.com>
2021-09-09 17:17:18 +0100

    iteration 4

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const hbs = require('hbs');
 const logger = require('morgan');
+const celebritiesRouter = require('./routes/celebrities');
 
 const app = express();
 
@@ -17,7 +18,8 @@ app.use(
   require('node-sass-middleware')({
     src: path.join(__dirname, 'public'),
     dest: path.join(__dirname, 'public'),
-    outputStyle: process.env.NODE_ENV === 'development' ? 'nested' : 'compressed',
+    outputStyle:
+      process.env.NODE_ENV === 'development' ? 'nested' : 'compressed',
     force: process.env.NODE_ENV === 'development',
     sourceMap: true
   })
@@ -33,6 +35,7 @@ app.use(favicon(path.join(__dirname, 'public/images/favicon.ico')));
 const baseRouter = require('./routes');
 
 app.use('/', baseRouter);
+app.use('/celebrities', celebritiesRouter);
 
 // Catch 404 and render a not-found.hbs template
 app.use((req, res, next) => {
---
+++ routes/celebrities.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const celebritiesRouter = express.Router();
+const Celebrity = require('../models/celebrity');
+
+celebritiesRouter.get('/create', (req, res, next) => {
+  res.render('celebrities/create');
+});
+
+celebritiesRouter.get('/', (req, res, next) => {
+  Celebrity.find()
+    .then((celebrities) => {
+      res.render('celebrities/index', { celebrities });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+celebritiesRouter.get('/:id', (req, res, next) => {
+  let id = req.params.id;
+  Celebrity.findById(id)
+    .then((celebrity) => {
+      res.render('celebrities/show', { celebrity });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+celebritiesRouter.post('/create', (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.create({
+    name,
+    occupation,
+    catchPhrase
+  })
+    .then((celebrity) => {
+      res.redirect('/');
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+module.exports = celebritiesRouter;
